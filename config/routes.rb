acc20afe296c2e5d09b593f4dc7901e8667d722b
Derek DiCillo <dddicillo@gmail.com>
2013-12-25 04:05:46 +0000

    Add styling and content

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Glcpa::Application.routes.draw do
-  get "static_pages/about"
-  get "static_pages/locations"
-  get "static_pages/contact"
-  get "static_pages/home"
-  get "static_pages/help"
+  root 'static_pages#home'
+  match '/about',     to: 'static_pages#about',     via: 'get'
+  match '/locations', to: 'static_pages#locations', via: 'get'
+  match '/contact',   to: 'static_pages#contact',   via: 'get'
+  match '/help',      to: 'static_pages#help',      via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
